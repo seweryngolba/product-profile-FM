9ec3d6a0b28c6fe48dd12b5f46712ab9d27e17da
Seweryn Golba <sgoolb@gmail.com>
2023-11-06 15:28:53 +0100

    cart counting

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const cart = document.querySelector(".cart");
 const cartProducts = document.querySelector(".cart-products");
 const cartDisplay = document.querySelector(".cart-display");
 const empty = document.querySelector(".empty");
+const priceCount = document.querySelector(".price-count");
+const total = document.querySelector(".total");
 
 let counter = 0;
 let cartItems = 0;
@@ -23,6 +25,11 @@ closeIcon.addEventListener("click", () => {
   mobileMenu.style.display = "none";
   overlay.style.display = "none";
 });
+
+const updateCart = () => {
+  priceCount.textContent = `$125.00 x ${cartItems}`;
+  total.textContent = `$${(cartItems * 125).toFixed(2)}`;
+};
 
 plus.addEventListener("click", () => {
   counter++;
@@ -38,11 +45,12 @@ minus.addEventListener("click", () => {
 
 addBtn.addEventListener("click", () => {
   if (counter === 0) {
-    cartProducts.style.display = "none";
+    return;
   } else {
     cartItems += counter;
     cartProducts.style.display = "block";
     cartProducts.textContent = cartItems;
+    updateCart();
   }
 });
 
@@ -53,3 +61,5 @@ cart.addEventListener("click", () => {
     cartDisplay.style.display = "flex";
   }
 });
+
+updateCart();
