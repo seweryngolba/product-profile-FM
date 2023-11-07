d408ee813569264b53adb46e6118483a1f7733ab
Seweryn Golba <sgoolb@gmail.com>
2023-11-07 15:42:20 +0100

    swiper mobile

--- main.js
+++ main.js
@@ -1,87 +1,99 @@
-const hamburgerMenu = document.querySelector(".ham-menu");
-const closeIcon = document.querySelector(".close-icon");
-const mobileMenu = document.querySelector(".categories");
-const overlay = document.querySelector(".overlay");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const productQuantity = document.querySelector(".product-quanitity");
-const addBtn = document.querySelector(".add-button");
-const cart = document.querySelector(".cart");
-const cartProducts = document.querySelector(".cart-products");
-const cartDisplay = document.querySelector(".cart-display");
-const empty = document.querySelector(".empty");
-const priceCount = document.querySelector(".price-count");
-const total = document.querySelector(".total");
-const orderSummary = document.querySelector(".order-summary");
-const checkoutBtn = document.querySelector(".checkout-button");
-const deleteProducts = document.querySelector(".delete-icon");
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
 
-let counter = 0;
-let cartItems = 0;
+    navigation: {
+      nextEl: ".next-image",
+      prevEl: ".previous-image",
+    },
+  });
 
-hamburgerMenu.addEventListener("click", () => {
-  mobileMenu.style.display = "flex";
-  overlay.style.display = "block";
-});
+  const hamburgerMenu = document.querySelector(".ham-menu");
+  const closeIcon = document.querySelector(".close-icon");
+  const mobileMenu = document.querySelector(".categories");
+  const overlay = document.querySelector(".overlay");
+  const plus = document.querySelector(".plus");
+  const minus = document.querySelector(".minus");
+  const productQuantity = document.querySelector(".product-quanitity");
+  const addBtn = document.querySelector(".add-button");
+  const cart = document.querySelector(".cart");
+  const cartProducts = document.querySelector(".cart-products");
+  const cartDisplay = document.querySelector(".cart-display");
+  const empty = document.querySelector(".empty");
+  const priceCount = document.querySelector(".price-count");
+  const total = document.querySelector(".total");
+  const orderSummary = document.querySelector(".order-summary");
+  const checkoutBtn = document.querySelector(".checkout-button");
+  const deleteProducts = document.querySelector(".delete-icon");
 
-closeIcon.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
-  overlay.style.display = "none";
-});
+  let counter = 0;
+  let cartItems = 0;
 
-deleteProducts.addEventListener("click", () => {
-  cartItems = 0;
-  orderSummary.style.display = "none";
-  checkoutBtn.style.display = "none";
-  empty.style.display = "flex";
-  cartProducts.style.display = "none";
-  updateCart();
-});
+  hamburgerMenu.addEventListener("click", () => {
+    mobileMenu.style.display = "flex";
+    overlay.style.display = "block";
+  });
 
-const updateCart = () => {
-  priceCount.textContent = `$125.00 x ${cartItems}`;
-  total.textContent = `$${(cartItems * 125).toFixed(2)}`;
+  closeIcon.addEventListener("click", () => {
+    mobileMenu.style.display = "none";
+    overlay.style.display = "none";
+  });
 
-  if (cartItems === 0) {
+  deleteProducts.addEventListener("click", () => {
+    cartItems = 0;
     orderSummary.style.display = "none";
     checkoutBtn.style.display = "none";
     empty.style.display = "flex";
-  } else {
-    orderSummary.style.display = "flex";
-    checkoutBtn.style.display = "block";
-    empty.style.display = "none";
-  }
-};
-
-plus.addEventListener("click", () => {
-  counter++;
-  productQuantity.textContent = counter;
-});
-
-minus.addEventListener("click", () => {
-  if (counter > 0) {
-    counter--;
-    productQuantity.textContent = counter;
-  }
-});
-
-addBtn.addEventListener("click", () => {
-  if (counter === 0) {
-    return;
-  } else {
-    cartItems += counter;
-    cartProducts.style.display = "block";
-    cartProducts.textContent = cartItems;
+    cartProducts.style.display = "none";
     updateCart();
-  }
-});
+  });
 
-cart.addEventListener("click", () => {
-  if (cartDisplay.style.display === "flex") {
-    cartDisplay.style.display = "none";
-  } else {
-    cartDisplay.style.display = "flex";
-  }
-});
+  const updateCart = () => {
+    priceCount.textContent = `$125.00 x ${cartItems}`;
+    total.textContent = `$${(cartItems * 125).toFixed(2)}`;
 
-updateCart();
+    if (cartItems === 0) {
+      orderSummary.style.display = "none";
+      checkoutBtn.style.display = "none";
+      empty.style.display = "flex";
+    } else {
+      orderSummary.style.display = "flex";
+      checkoutBtn.style.display = "block";
+      empty.style.display = "none";
+    }
+  };
+
+  plus.addEventListener("click", () => {
+    counter++;
+    productQuantity.textContent = counter;
+  });
+
+  minus.addEventListener("click", () => {
+    if (counter > 0) {
+      counter--;
+      productQuantity.textContent = counter;
+    }
+  });
+
+  addBtn.addEventListener("click", () => {
+    if (counter === 0) {
+      return;
+    } else {
+      cartItems += counter;
+      cartProducts.style.display = "block";
+      cartProducts.textContent = cartItems;
+      updateCart();
+    }
+  });
+
+  cart.addEventListener("click", () => {
+    if (cartDisplay.style.display === "flex") {
+      cartDisplay.style.display = "none";
+    } else {
+      cartDisplay.style.display = "flex";
+    }
+  });
+
+  updateCart();
+});
